3dcd2f1040f91e12bdd19c8fad8b6c2c9c1f2077
vitor <contato10@bulloleo.com.br>
2024-05-22 21:48:51 -0300

    nome projeto e validar 8 caracateres

--- pj01/lib/screens/registro.dart
+++ pj01/lib/screens/registro.dart
@@ -11,6 +11,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>(); // Chave para o Form
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -35,176 +36,186 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding:
-              const EdgeInsets.symmetric(horizontal: 25.0, vertical: 100.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _nameController,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  labelText: 'Nome',
-                  labelStyle: TextStyle(color: Colors.white),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                controller: _emailController,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.white),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                controller: _passwordController,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: Colors.white),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField( // Novo campo de confirmação de senha
-                controller: _confirmPasswordController,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  labelText: 'Confirmar Senha',
-                  labelStyle: TextStyle(color: Colors.white),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 20.0),
-              const SizedBox(height: 20.0),
-              Row(
-                children: [
-                  Checkbox(
-                    value: _acceptedTerms,
-                    onChanged: (bool? newValue) {
-                      setState(() {
-                        _acceptedTerms = newValue!;
-                      });
-                    },
-                  ),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: _launchURL,
-                      child: const Text(
-                        'Aceito os termos de serviço',
-                        style: TextStyle(color: Colors.white, decoration: TextDecoration.underline),
-                      ),
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 100.0),
+          child: Form(
+            key: _formKey, // Utilizando o Form com a chave
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Nome',
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                   ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  if (!_acceptedTerms) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Você precisa aceitar os termos de serviço para se registrar."),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  controller: _emailController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  controller: _passwordController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (senha) {
+                    if (senha == null || senha.isEmpty) {
+                      return "Digite sua Senha !";
+                    } else if (senha.length < 8) {
+                      return 'A senha deve ter pelo menos 8 caracteres';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    labelText: 'Confirmar Senha',
+                    labelStyle: TextStyle(color: Colors.white),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                  obscureText: true,
+                  validator: (confirmSenha) {
+                    if (confirmSenha == null || confirmSenha.isEmpty) {
+                      return "Confirme sua Senha !";
+                    } else if (_passwordController.text != confirmSenha) {
+                      return 'As senhas não coincidem';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _acceptedTerms,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          _acceptedTerms = newValue!;
+                        });
+                      },
+                    ),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: _launchURL,
+                        child: const Text(
+                          'Aceito os termos de serviço',
+                          style: TextStyle(color: Colors.white, decoration: TextDecoration.underline),
+                        ),
                       ),
-                    );
-                    return;
-                  }
-                  try {
-                    if (_passwordController.text !=
-                        _confirmPasswordController.text) {
-                      throw 'A senha e a confirmação de senha não coincidem';
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      if (!_acceptedTerms) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Você precisa aceitar os termos de serviço para se registrar."),
+                          ),
+                        );
+                        return;
+                      }
+                      register(_nameController.text, _emailController.text, _passwordController.text)
+                        .then((success) {
+                          if (success) {
+                            Navigator.pushReplacementNamed(context, '/login');
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Falha ao se registrar, tente novamente.")),
+                            );
+                          }
+                        }).catchError((e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(e.toString())),
+                          );
+                        });
                     }
-                    var success = await register(
-                        _nameController.text,
-                        _emailController.text,
-                        _passwordController.text);
-                    if (success) {
-                      Navigator.pushReplacementNamed(context, '/login');
-                    } else {
-                      // Handle unsuccessful registration
-                    }
-                  } catch (e) {
-                    // Exibe a mensagem de erro
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(e.toString())),
-                    );
-                  }
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  child: const Text(
+                    'Cadastrar',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                child: const Text(
-                  'Cadastrar',
-                  style: TextStyle(color: Colors.white),
+                const SizedBox(height: 20.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                  ),
+                  child: const Text(
+                    'Já possui uma conta?\nClique aqui para fazer login',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/login');
-                },
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                child: const Text(
-                  'Já possui uma conta?\nClique aqui para fazer login',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-}
 
-Future<bool> register(String name, String email, String password) async {
-  final url = Uri.parse(
-      'http://172.31.41.95:3001/users/register?apiKey=grupo8_falaAI');
-  final body = jsonEncode({'name': name, 'email': email, 'password': password});
+  Future<bool> register(String name, String email, String password) async {
+    final url = Uri.parse('http://172.28.144.1:3001/users/register?apiKey=grupo8_falaAI');
+    final body = jsonEncode({'name': name, 'email': email, 'password': password});
 
-  try {
-    final response = await http.post(
-      url,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: body,
-    );
+    try {
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: body,
+      );
 
-    print('Resposta do servidor:');
-    print('Status: ${response.statusCode}');
-    print('Corpo: ${response.body}');
+      print('Resposta do servidor:');
+      print('Status: ${response.statusCode}');
+      print('Corpo: ${response.body}');
 
-    if (response.statusCode == 201) {
-      print('Registro bem-sucedido');
-      return true;
-    } else if (response.statusCode == 400) {
-      throw 'Os dados fornecidos são inválidos';
-    } else if (response.statusCode == 409) {
-      throw 'Já existe um usuário com este e-mail';
-    } else {
-      throw 'Erro desconhecido durante o registro';
+      if (response.statusCode == 201) {
+        print('Registro bem-sucedido');
+        return true;
+      } else if (response.statusCode == 400) {
+        throw 'Os dados fornecidos são inválidos';
+      } else if (response.statusCode == 409) {
+        throw 'Já existe um usuário com este e-mail';
+      } else {
+        throw 'Erro desconhecido durante o registro';
+      }
+    } catch (e) {
+      throw 'Erro ao fazer registro: $e';
     }
-  } catch (e) {
-    throw 'Erro ao fazer registro: $e';
   }
 }
